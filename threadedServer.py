01c78034383bbb0920a0acb074f1a0ec0b9afa69
Elliot Penn <epenn28@vt.edu>
2017-07-05 16:51:28 -0400

    Updated server with stop function

--- threadedServer.py
+++ threadedServer.py
@@ -10,6 +10,7 @@ class ThreadedServer(object):
 	def __init__(self, host, port):
 		self.host = host
 		self.port = port
+		self.running = True
 		self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 		self.sock.bind((self.host, self.port))
@@ -17,11 +18,10 @@ class ThreadedServer(object):
 	def listen(self):
 		self.sock.listen(5)
 		print ('Server started and listening')
-		while True:
+		while self.running:
 			client, address = self.sock.accept()
 			client.settimeout(60)
 			client_thread = threading.Thread(target = self.listenToClient, args = (client, address))
-			client_thread.daemon = True
 			client_thread.start()
 
 	def listenToClient(self, client, address):
@@ -41,13 +41,18 @@ class ThreadedServer(object):
 					response = data
 					client.send(response)
 				else:
-					raise Exception('Client disconnected')
-			except KeyboardInterrupt:
-				dataToSend = "0F000000F0"
-				response = binascii.unhexlify(dataToSend)
-				client.send(response)
-				client.close()
+					raise Exception('Client disconnected')  # this occurs when socket.close happens on the client side
+			except Exception as e:
+				#dataToSend = "0F000000F0"
+				#response = binascii.unhexlify(dataToSend)
+				#client.send(response)
+				#client.close()
+				print("Stopping server, reason:", e)
+				self.stopServer()
 				return False
+				
+	def stopServer(self):
+		self.running = False
 
 if __name__ == "__main__":
 	port_num = 2000
