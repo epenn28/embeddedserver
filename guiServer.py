2e031f0759d16844fa7acde9d26736d10ded9948
Elliot Penn <epenn28@vt.edu>
2017-07-07 15:09:25 -0400

    Basic start/stop button working

--- guiServer.py
+++ guiServer.py
@@ -4,11 +4,13 @@ import select
 import threading
 import codecs
 import binascii
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QMessageBox
+from PyQt5.QtNetwork import QTcpServer, QTcpSocket, QAbstractSocket, QHostAddress
+from PyQt5.QtCore import QThread, QReadWriteLock, QDataStream, pyqtSignal
 from ui_mainwindow import Ui_MainWindow
 
-class ThreadedServer(object):
-	def __init__(self, host, port):
+
+"""
 		self.host = host
 		self.port = port
 		self.stopPressed = False
@@ -61,16 +63,62 @@ class ThreadedServer(object):
 	def stopServer(self):
 		self.stopPressed = True
 		print("Stopping server")
+"""
+		
+		
+class Thread(QThread):
+	
+	trigger = pyqtSignal(bytes)
+	
+	def __init__(self, socketId, parent = None):
+		super().__init__(parent)
+		self.socketId = socketId
+		
+	def run(self):
+		socket = QTcpSocket()
+		if not socket.setSocketDescriptor(self.socketId):
+			self.emit(SIGNAL("error(int)"), socket.error())
+			return
+		while socket.state() == QAbstractSocket.ConnectedState:
+			data = bytes(5)
+			stream = QDataStream(socket)
+			stream.setVersion(QDataStream.Qt_5_8)
+			while True:
+				socket.waitForReadyRead(-1)
+				if socket.bytesAvailable() >= 5:
+					data = stream.readRawData(5)
+					self.trigger.emit(data)
+					break
+		
+		
+class ThreadedServer(QTcpServer):
+	def __init__(self, parent = None):
+		super().__init__(parent)
+		
+	def incomingConnection(self, socketId):
+		thread = Thread(socketId, self)
+		#self.connect(thread, SIGNAL("finished()"), thread, SLOT("deleteLater()"))
+		thread.start()
 
         
 class MyWindow(QMainWindow):
-	def __init__(self):
-		super().__init__()
+	def __init__(self, parent = None):
+		super().__init__(parent)
 		self.ui = Ui_MainWindow()
 		self.ui.setupUi(self)
-		self.server = ThreadedServer('', port_num)
-		self.ui.startButton.clicked.connect(self.server.listen)
-		self.ui.stopButton.clicked.connect(self.server.stopServer)
+		
+		# Set up server
+		self.server = ThreadedServer(self)
+		
+		# Connect buttons
+		self.ui.startButton.clicked.connect(self.startServer)
+		self.ui.stopButton.clicked.connect(self.server.close)
+		
+	def startServer(self):
+		if not self.server.listen(QHostAddress("0.0.0.0"), port_num):
+			QMessageBox.critical(self, "Raspberry Pi server", QString("Failed to start server: %1").arg(self.server.errorString()))
+			self.close()
+			return
 
 		
 port_num = 2000
